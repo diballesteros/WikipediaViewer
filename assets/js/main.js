5d1a8b19f041a387a7871002c2423c5ce914f473
Diego Ballesteros <diballesterosc@gmail.com>
2018-02-16 17:39:48 -0500

    API call and HTML Structure
    -HTML: Random page link
    -HTML: Input for search
    -HTML: Structure for search results
    -HTML: Bootstrap Body Structure
    -JS:  Wikipedia API CALL
    -JS: Search upon entering search item

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    var results = document.getElementById("results");
+
+
+    $(document).on('keyup', function (e) {
+        if ($("#search").is(":focus") && e.key == "Enter") {
+
+
+
+            var call = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&list=search&rvprop=content&srsearch="
+                + document.getElementById("search").value + "&srlimit=15&srprop=snippet";
+
+            var html = '';
+
+            var urly = "https://en.wikipedia.org/?curid=";
+
+            $.getJSON(call, function (json) {
+                json.query.search.forEach(function (val) {
+                    html += "<li>" + val.title + "</li>";
+                });
+                results.innerHTML = html;
+            });
+
+
+        }
+    });
+});
