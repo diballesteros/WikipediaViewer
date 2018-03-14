906e9ba52c076d6404c065e65faab86939d302a6
Diego Ballesteros <diballesterosc@gmail.com>
2018-03-13 22:43:14 -0500

    New Structure
    -Remove Bootstrap
    -Remove Angular
    -Include React

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,42 @@
-$(document).ready(function () {
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>This is a Test</h1>
+            </div>
+        );
+    }
+}
 
-    var app = angular.module('WikiViewer', []);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    app.controller('WikiController', function () {
-
-    });
-
-    var results = document.getElementById("results");
-
-
-    $(document).on('keyup', function (e) {
-        if ($("#search").is(":focus") && e.key == "Enter") {
-
-
-
-            var call = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&list=search&rvprop=content&srsearch="
-                + document.getElementById("search").value + "&srlimit=15&srprop=snippet";
-
-            var html = '';
-
-            var urly = "https://en.wikipedia.org/?curid=";
-
-            $.getJSON(call, function (json) {
-                json.query.search.forEach(function (val) {
-                    html += "<li>" + val.title + "</li>";
-                });
-                results.innerHTML = html;
-            });
+// $(document).ready(function () {
 
 
-        }
-    });
+//     var results = document.getElementById("results");
 
-});
+//     $(document).on('keyup', function (e) {
+//         if ($("#search").is(":focus") && e.key == "Enter") {
+
+
+
+//             var call = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=revisions&list=search&rvprop=content&srsearch="
+//                 + document.getElementById("search").value + "&srlimit=15&srprop=snippet";
+
+//             var html = '';
+
+//             var urly = "https://en.wikipedia.org/?curid=";
+
+//             $.getJSON(call, function (json) {
+//                 json.query.search.forEach(function (val) {
+//                     html += "<li>" + val.title + "</li>";
+//                 });
+//                 results.innerHTML = html;
+//             });
+
+
+//         }
+//     });
+
+
+// });
